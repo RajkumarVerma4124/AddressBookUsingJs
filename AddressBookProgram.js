ee48db3c386ca89dd28718a1fcb394dbde20f0c1
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-03-11 15:21:07 +0530

    [Raj] Added:- The contact array to add mul contact

--- AddressBookProgram.js
+++ AddressBookProgram.js
@@ -15,13 +15,29 @@ class Contact {
 
     //Initializing the parameterized constructor of class using constructor keyword(UC1)
     constructor(...parameters) {
+        //Regex patterns for validating contact details(UC2&&UC3)
+        let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
+        let addressPattern = new RegExp('^[A-Za-z0-9]{4,}$');
+        let zipCodePattern = new RegExp('^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$');
+        let phoneNumPattern = new RegExp('^\\+?91[ ]?[1-9][0-9]{9}$');
+        let emailIdPattern = new RegExp('^[a-zA-Z0-9]{3,}([._+-][0-9a-zA-Z]{2,})*@[0-9a-zA-Z]+[.]?([a-zA-Z]{2,4})+[.]?([a-zA-Z]{2,3})*$');
+        if (!namePattern.test(parameters[0]) && !namePattern.test(parameters[1]))
+            throw 'First or last name should have minimum 3 characters';
         this.firstName = parameters[0];
         this.lastName = parameters[1];
+        if (!addressPattern.test(parameters[2]) && !addressPattern.test(parameters[3]) && !addressPattern.test(parameters[4]))
+            throw 'It Should have minimum 4 characters';
         this.address = parameters[2];
         this.city = parameters[3];
         this.state = parameters[4];
+        if (!zipCodePattern.test(parameters[5]))
+            throw 'Zipcode is not valid';
         this.zipCode = parameters[5];
+        if (!phoneNumPattern.test(parameters[6]))
+            throw 'Phone number is not valid';
         this.phoneNumber = parameters[6];
+        if (!emailIdPattern.test(parameters[7]))
+            throw 'Email id is not valid';
         this.emailId = parameters[7];
     }
 
@@ -31,11 +47,14 @@ class Contact {
     }
 }
 
+//Initializing an addressbook contact array(UC3)
+var addressBookContactArr = new Array();
+
 //Displaying the welcome message
 console.log("Welcome To The AddressBook Program Using Js")
 
 //Function to return object of added contacts(UC1)
-function returnAddedContact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId) {
+function addContact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId) {
     let contact
     try {
         //Object for class
@@ -43,57 +62,78 @@ function returnAddedContact(firstName, lastName, address, city, state, zipCode, 
     } catch (e) {
         console.error(e)
     }
-    return contact;
+    addressBookContactArr.push(contact);
 }
 
-//Function to get the details of the contact from the user(UC1 && UC2)
-function getContactDetails() {
+//Fucntion to add default contact into an array(C3)
+function addDefaultContactDetails() {
     try {
-        //Regex patterns for validating contact details(UC2)
-        let namePattern = new RegExp('^[A-Z]{1}[a-z]{2,}$');
-        let addressPattern = new RegExp('^[A-Za-z]{4,}$');
-        let zipCodePattern = new RegExp('^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$');
-        let phoneNumPattern = new RegExp('^\\+?91[ ]?[1-9][0-9]{9}$');
-        let emailIdPattern = new RegExp('^[a-zA-Z0-9]{3,}([._+-][0-9a-zA-Z]{2,})*@[0-9a-zA-Z]+[.]?([a-zA-Z]{2,4})+[.]?([a-zA-Z]{2,3})*$');
-        //Validating first name(UC2)
-        let firstName = prompt('Enter Your FirstName Start With Capital : ');
-        if (!namePattern.test(firstName))
-            throw 'First name should have minimum 3 characters';
-        //Validating last name(UC2)
-        let lastName = prompt('Enter Your LastName Start With Capital : ');
-        if (!namePattern.test(lastName))
-            throw 'First name should have minimum 3 characters';
-        //Validating address(UC2)
-        let address = prompt('Enter Your Address : ');
-        if (!addressPattern.test(address))
-            throw 'Address should have minimum 4 characters';
-        //Validating city name(UC2)
-        let city = prompt('Enter Your City Name: ');
-        if (!addressPattern.test(city))
-            throw 'City should have minimum 4 characters';
-        //Validating states name(UC2)
-        let state = prompt('Enter Your State Name : ');
-        if (!addressPattern.test(state))
-            throw 'State should have minimum 4 characters';
-        //Validating zip code(UC2)
-        let zipCode = prompt('Enter Your Zip Code : ');
-        if (!zipCodePattern.test(zipCode))
-            throw 'Zipcode is not valid';
-        //Validating phone number(UC2)
-        let phoneNumber = prompt('Enter Your Phone Number : ');
-        if (!phoneNumPattern.test(phoneNumber))
-            throw 'Phone number is not valid';
-        //Validating email id(UC2)
-        let emailId = prompt('Enter Your Email Id : ');
-        if (!emailIdPattern.test(emailId))
-            throw 'Email id is not valid';
-        let contactDetails = returnAddedContact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId);
-        console.log("\nDetails Of Contacts");
-        console.log(contactDetails.toString());
+        addContact("Rajkumar", "Verma", "Ghansoli", "NaviMumbai", "Maharashtra", "456123", "91 9517534561", "raj123@gmail.com");
+        addContact("Yash", "Verma", "Sec-45", "Noida", "Delhi", "789456", "91 7412589631", "yash456@gmail.com");
+        addContact("Ajay", "Matkar", "Chembur", "Mumbai", "Maharashtra", "456123", "91 8523697412", "ajay789@gmail.com");
+        addContact("Aman", "Nikam", "Boriwali", "Mumbai", "Maharashtra", "401 567", "918562497412", "aman789@gmail.com");
+        if (addressBookContactArr.length != 0)
+            console.log("Added The Default Contacts Succesfully");
     } catch (e) {
         console.error(e);
     }
 }
 
-//Calling the function(UC1)
-getContactDetails();
+//Function to get the details of the contact from the user(UC1 && UC2)
+function getContactDetails() {
+    try {
+        let firstName = prompt('Enter Your FirstName Start With Capital : ');
+        let lastName = prompt('Enter Your LastName Start With Capital : ');
+        let address = prompt('Enter Your Address : ');
+        let city = prompt('Enter Your City Name: ');
+        let state = prompt('Enter Your State Name : ');
+        let zipCode = prompt('Enter Your Zip Code : ');
+        let phoneNumber = prompt('Enter Your Phone Number : ');
+        let emailId = prompt('Enter Your Email Id : ');
+        addContact(firstName, lastName, address, city, state, zipCode, phoneNumber, emailId);
+        console.log("Added The Contact Succesfully");
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+//Function to display contacts(UC3)
+function displayContact() {
+    try {
+        console.log("\n**************Details Of AddressBook Contacts*********************\n");
+        addressBookContactArr.forEach(contact => console.log(contact.toString()));
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+//Function to perform addressbook operations(UC3)
+function addressBookOperations() {
+    try {
+        while (true) {
+            console.log("1: Add New Contact \n2: Add Default Contacts \n3: Display \n4: Exit");
+            switch (parseInt(prompt('Enter the choice : '))) {
+                case 1:
+                    getContactDetails();
+                    break;
+                case 2:
+                    addDefaultContactDetails();
+                    break;
+                case 3:
+                    displayContact();
+                    break;
+                case 4:
+                    console.log("Exited");
+                    process.exit(1)
+                    break;
+                default:
+                    console.log("Wrong Choice");
+                    break;
+            }
+        }
+    } catch (e) {
+        console.error(e);
+    }
+}
+//Calling the addressbook operation functions(UC3)
+addressBookOperations();
